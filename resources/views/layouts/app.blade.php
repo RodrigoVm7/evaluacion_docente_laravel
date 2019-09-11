6266f35aea1b1ca078d77f02290ee729f60189cd
Carlos <karlooohz97@gmail.com>
2019-09-11 16:00:19 -0400

    versioncarlos2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Evaluacion Docentexd</title>
+    <title>Evaluacion Docent</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
