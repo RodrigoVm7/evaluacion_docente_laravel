80d32b5e3a86f2cc8107eb131dd79e28b6d6cc39
marceloR <marceloroa19@gmail.com>
2019-09-11 18:52:07 +0200

    version4

--- resources/views/respuestas/index.blade.php
+++ resources/views/respuestas/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container">
-h
+hhgsaa
 <!-- Sección que permite mostrar mensajes en pantalla-->
 @if(Session::has('Mensaje'))
 <div class="alert alert-success" role="alert">
